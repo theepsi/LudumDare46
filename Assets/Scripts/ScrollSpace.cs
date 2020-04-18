51ba153454458ee439f94d9a3123aac90589c6bd
Laura Martínez Sanahuja <lauramartinezsanahuja@gmail.com>
2020-04-18 18:03:46 +0200

    Add Parallax in stars following camera movement

---
+++ Assets/Scripts/ScrollSpace.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollSpace : MonoBehaviour
+{
+    Material meshRendererMaterial;
+
+    public float parallax = 2f;
+
+    private void Start()
+    {
+        meshRendererMaterial = GetComponent<MeshRenderer>().material;
+    }
+
+    void Update()
+    {
+        Vector2 offset = meshRendererMaterial.mainTextureOffset;
+
+        offset.x = transform.position.x / transform.localScale.x / parallax;
+        offset.y = transform.position.y / transform.localScale.y / parallax;
+
+        meshRendererMaterial.mainTextureOffset = offset;
+    }
+}
