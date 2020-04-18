f14c6fb90e639eb3373956ad1fec928b6283305d
Ennio Casas <theepsi@gmail.com>
2020-04-18 15:59:27 +0200

    Add simple player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(BoxCollider))]
+public class PlayerController : MonoBehaviour
+{
+    public float impulseSpeed = 2.2f;
+    public float rotateSpeed = 85f;
+
+    private Rigidbody mRigidbody;
+
+    void Start()
+    {
+        mRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        RotateShip(Input.GetAxis("Horizontal"));
+
+        ImpulseShip(Input.GetAxis("Vertical"));
+    }
+
+    private void RotateShip(float direction)
+    {
+        if (direction != 0)
+        {
+            Quaternion rotation = Quaternion.AngleAxis(direction * rotateSpeed * Time.deltaTime, Vector3.up);
+            transform.forward = rotation * transform.forward;
+        }
+    }
+
+    private void ImpulseShip(float acceleration)
+    {
+        if (acceleration != 0)
+        {
+            mRigidbody.velocity += transform.forward * impulseSpeed * Time.deltaTime * acceleration;
+        }
+    }
+}
