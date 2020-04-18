aae15a851436c3a0deeab8c5d65e860c19424fef
Ennio Casas <theepsi@gmail.com>
2020-04-18 16:23:13 +0200

    Add camera follow logic

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float followSpeed = 10f;
+
+    void FixedUpdate()
+    {
+        if (target != null)
+        {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, followSpeed * Time.deltaTime);
+            transform.position = smoothedPosition;
+        }
+    }
+}
