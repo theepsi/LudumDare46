5d2216f357970a8e30fa569e0eff9e7d6fcc502d
Ennio Casas <theepsi@gmail.com>
2020-04-20 20:58:44 +0200

    Finish Main UI

---
+++ Assets/Scripts/Managers/MenuButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        EffectsHelper.SFX("UI/onButtonClick");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        EffectsHelper.SFX("UI/onButtonEnter");
+        transform.DOScale(1.1f, 0.5f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        EffectsHelper.SFX("UI/onButtonExit");
+        transform.DOScale(1, 0.5f);
+    }
+}
