a12de6d38fdd2015b3b69e2b27a4a69ad5b05567
Laura Martínez Sanahuja <lauramartinezsanahuja@gmail.com>
2020-04-20 17:43:43 +0200

    add modules sfx

--- Assets/Scripts/Effects/EffectsHelper.cs
+++ Assets/Scripts/Effects/EffectsHelper.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 
 public static class EffectsHelper
 {
-    public static void SFX(string name)
+    public static float SFX(string name)
     {
-        AudioSource crash = ObjectPooler.Instance.GetPooledObject("AudioSource").GetComponent<AudioSource>();
-        crash.clip = Resources.Load<AudioClip>("SFX/"+ name);
-        crash.gameObject.SetActive(true);
-        crash.Play();
+        AudioSource sfx = ObjectPooler.Instance.GetPooledObject("AudioSource").GetComponent<AudioSource>();
+        sfx.clip = Resources.Load<AudioClip>("SFX/"+ name);
+        sfx.gameObject.SetActive(true);
+        sfx.Play();
+        return sfx.clip.length;
     }
     public static void Particles(string name, Vector3 position)
     {
-        ParticleSystem moduleCrash = ObjectPooler.Instance.GetPooledObject(name).GetComponent<ParticleSystem>();
-        moduleCrash.transform.position = position;
-        moduleCrash.gameObject.SetActive(true);
-        moduleCrash.Play();
+        ParticleSystem particleSystem = ObjectPooler.Instance.GetPooledObject(name).GetComponent<ParticleSystem>();
+        particleSystem.transform.position = position;
+        particleSystem.gameObject.SetActive(true);
+        particleSystem.Play();
     }
 
     public static AudioSource Music(string name) {
